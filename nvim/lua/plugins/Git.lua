acb64919f449e153587aa3dbd683f8f2ce81da99
Ganesh <ganeshsonie@outlook.com>
2026-02-21 14:13:42 +0530

    improved git stuff

--- nvim/lua/plugins/Git.lua
+++ nvim/lua/plugins/Git.lua
@@ -60,71 +60,118 @@ return {
 				GLRUCL = "┴",
 				GLRUCR = "┴",
 			},
+
 			hooks = {
 				on_select_commit = function(commit)
 					local gg = require("gitgraph")
 					local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
 					local current_br = vim.fn.systemlist("git branch --show-current")[1]
+					local current_head = vim.fn.systemlist("git -C " .. git_root .. " rev-parse HEAD")[1]
 					local full_hash = vim.fn.systemlist("git -C " .. git_root .. " rev-parse " .. commit.hash)[1]
 
 					local function run(cmd)
 						local output = vim.fn.system("git -C " .. git_root .. " " .. cmd)
-						vim.notify(output ~= "" and output or "Action successful")
+						local clean = output:gsub("^%s*(.-)%s*$", "%1")
+						vim.api.nvim_echo(
+							{ { "\nGit: ", "Identifier" }, { clean ~= "" and clean or "Action successful", "Normal" } },
+							true,
+							{}
+						)
 						gg.draw({}, { all = true, max_count = 1000 })
 					end
 
-					local options = { " Diff Split", "󰅍 Copy Hash", " Create New Branch", " Cherry-pick" }
-
-					-- Use the first branch name in the list if it exists
 					local target_branch = (commit.branch_names and #commit.branch_names > 0) and commit.branch_names[1]
 						or nil
+					if target_branch then
+						target_branch = target_branch:gsub("HEAD %-> ", "")
+					end
+
+					local is_head = (full_hash == current_head)
+					local is_curr_br = (target_branch == current_br)
+
+					local menu = {
+						{ key = "d", label = " Diff Split", id = "diff" },
+						{ key = "y", label = "󰅍 Copy Hash", id = "copy" },
+						{ key = "n", label = " Create New Branch", id = "create" },
+					}
+					if not is_head then
+						table.insert(menu, { key = "p", label = " Cherry-pick", id = "cherry" })
+					end
 
 					if target_branch then
-						if target_branch ~= current_br then
-							table.insert(options, " Checkout Branch: " .. target_branch)
-							table.insert(options, " Merge " .. target_branch)
-							table.insert(options, "󰆴 Delete Branch: " .. target_branch)
-						else
-							table.insert(options, "󰑕 Rename Branch")
+						if not is_curr_br then
+							table.insert(
+								menu,
+								{ key = "c", label = " Checkout Branch: " .. target_branch, id = "checkout" }
+							)
+							if not is_head then
+								table.insert(menu, { key = "m", label = " Merge " .. target_branch, id = "merge" })
+							end
+							table.insert(
+								menu,
+								{ key = "x", label = "󰆴 Delete Branch: " .. target_branch, id = "delete" }
+							)
+						end
+						table.insert(menu, {
+							key = "r",
+							label = "󰑕 Rename Branch: " .. (is_curr_br and "Current" or target_branch),
+							id = "rename",
+						})
+						if
+							is_curr_br
+							and vim.fn.system("git rev-parse --abbrev-ref " .. target_branch .. "@{u}"):match("fatal")
+						then
+							table.insert(menu, { key = "u", label = " Push Branch (Set Upstream)", id = "push" })
 						end
 					end
 
-					vim.ui.select(
-						options,
-						{ prompt = "Git Action (" .. commit.hash:sub(1, 7) .. "):" },
-						function(choice)
-							if not choice then
-								return
-							end
+					local display = { { "Git Action (" .. commit.hash:sub(1, 7) .. "):\n", "Title" } }
+					for _, item in ipairs(menu) do
+						table.insert(display, { string.format("[%s]", item.key), "WarningMsg" })
+						table.insert(display, { "   " .. item.label .. "\n", "Normal" })
+					end
+					vim.api.nvim_echo(display, true, {})
 
-							if choice == " Create New Branch" then
-								local name = vim.fn.input("New branch name: ")
-								if name ~= "" then
-									run("checkout -b " .. name .. " " .. full_hash)
-								end
-							elseif choice:match("Checkout Branch:") then
-								run("checkout " .. target_branch)
-							elseif choice:match("Merge") then
-								run("merge " .. target_branch)
-							elseif choice == " Cherry-pick" then
-								run("cherry-pick " .. full_hash)
-							elseif choice == " Diff Split" then
-								vim.cmd("DiffviewOpen " .. full_hash .. "^!")
-							elseif choice == "󰅍 Copy Hash" then
-								vim.fn.setreg("+", full_hash)
-							elseif choice:match("Delete") then
-								local confirm = vim.fn.input("Delete " .. target_branch .. "? (y/N): ")
-								if confirm == "y" then
-									run("branch -D " .. target_branch)
-								end
-							elseif choice:match("Rename") then
-								local n = vim.fn.input("New name: ")
-								if n ~= "" then
-									run("branch -m " .. n)
-								end
+					local char = vim.fn.getcharstr()
+					local action = nil
+					for _, item in ipairs(menu) do
+						if item.key == char then
+							action = item.id
+							break
+						end
+					end
+
+					if action == "diff" then
+						vim.cmd("DiffviewOpen " .. full_hash .. "^!")
+					elseif action == "copy" then
+						vim.fn.setreg("+", full_hash)
+					elseif action == "create" then
+						local name = vim.fn.input("New branch name: ")
+						if name ~= "" then
+							if vim.fn.input("Checkout " .. name .. "? (y/N): ") == "y" then
+								run(string.format("checkout -b %s %s", name, full_hash))
+							else
+								run(string.format("branch %s %s", name, full_hash))
 							end
 						end
-					)
+					elseif action == "cherry" then
+						run("cherry-pick " .. full_hash)
+					elseif action == "checkout" then
+						run("checkout " .. target_branch)
+					elseif action == "merge" then
+						run("merge " .. target_branch)
+					elseif action == "push" then
+						run("push -u origin " .. current_br)
+					elseif action == "delete" then
+						if vim.fn.input("Delete " .. target_branch .. "? (y/N): ") == "y" then
+							run("branch -D " .. target_branch)
+						end
+					elseif action == "rename" then
+						local n = vim.fn.input("New name: ")
+						if n ~= "" then
+							run("branch -m " .. (is_curr_br and "" or target_branch .. " ") .. n)
+						end
+					end
 				end,
 			},
 		},
